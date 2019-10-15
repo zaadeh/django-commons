09ee21190c2179f4996cd653229f2436e8531b59
Ali Alizadeh <a.alizadeh@rayanbourse.ir>
2019-10-15 12:34:05 +0330

    added a sample json encoder for decimal type

---
+++ commons/serialization.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals, print_function, absolute_import
+
+import json
+from decimal import Decimal
+
+
+class DecimalEncoder(json.JSONEncoder):
+    """json encoder that can handle `Decimal` type
+
+    Usage:
+
+        d = Decimal('42.5')
+        json.dumps(d, cls=DecimalEncoder)
+
+    Django includes a more complete JSON encoder at
+    `django.core.serializers.DjangoJSONEncoder` that in addition to decimals,
+    can handle `date`, `time`, `datetime`, `UUID`, etc.
+    """
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
